a9cb88670cca07ce1deb69f39a4d5f7e749945a4
Jonathan Lahue <jonathanlahue@gmail.com>
2022-08-31 21:48:34 -0400

    some minor styling of the intro pannel

--- src/components/introPanel.js
+++ src/components/introPanel.js
@@ -6,6 +6,12 @@ class IntroPanel extends Component {
         return (
             <div className="intro-panel">
                 <OrnateLabel>Word Tagging Machine</OrnateLabel>
+                <div>
+                    <p>What values would you associate with the word “broccoli”? What about “spaghetti”, or “clementine”, or “doorknob”?</p>
+                    <p>This machine lets you associate different values to words, to make the name generating machine smarter!</p>
+                    <p>Happy word tagging!</p>
+                </div>
+
             </div>
             
         )
